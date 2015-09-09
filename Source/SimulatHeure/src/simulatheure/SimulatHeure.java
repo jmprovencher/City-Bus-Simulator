bbcd4aea49b4b09011ebd93e5eab29001f5149de
UncleSam <s.c_123@hotmail.com>
2015-09-09 19:11:30 -0400

    Modif du diagramme UML. Ajout de relations entre les objects. Création des classes Java (vides)

---
+++ Source/SimulatHeure/src/simulatheure/SimulatHeure.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simulatheure;
+import javax.swing.*;
+
+/**
+ *
+ * @author Sam
+ */
+public class SimulatHeure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new JFrame("Mom I'm doing Java!");
+        frame.setSize(600, 600);
+        frame.show();
+    }
+    
+}
